35c90736e7f781dbcc55ec9254db26674cd9d3b1
sasi921 <124865456+sasi921@users.noreply.github.com>
2023-05-01 23:23:45 +0530

    Create recomender.py

---
+++ recomender.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+# globalstart = datetime.now()
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use('nbagg')
+
+import matplotlib.pyplot as plt
+plt.rcParams.update({'figure.max_open_warning': 0})
+
+import seaborn as sns
+sns.set_style('whitegrid')
+import os
+from scipy import sparse
+from scipy.sparse import csr_matrix
+
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import cosine_similarity
+import random
